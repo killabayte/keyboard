0cd0b5de816286e87610ef2d7f8679ccd54a5e8c
Yaroslav Kostiannikov <yaroslav.kostiannikov@openvpn.com>
2023-07-26 10:40:07 +0200

    Go packages example, v0.3

--- keyboard.go
+++ keyboard.go
@@ -9,6 +9,7 @@ import (
 )
 
 // GetFloat reads a floating-point number from keyboard.
+// It returns the number read and any error encountered
 func GetFloat() (float64, error) {
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
